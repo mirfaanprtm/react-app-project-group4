81adeaf7072259a1514a57f5eae7adf192d85680
Rohma <dhurrohma28@gmail.com>
2023-04-08 11:43:51 +0700

    Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./pages/Header";
 import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Footer />
     </>
   );
 }
